366eae5516d9c0269ef0bb541c2443531e8db986
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-20 13:00:43 -0400

    Added models and registered them with admin

--- symptoms_api/api/admin.py
+++ symptoms_api/api/admin.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Symptom, Diagnosis, DiagnosisToSymptom
+
+
+@admin.register(Symptom)
+class SymptomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Diagnosis)
+class DiagnosisAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DiagnosisToSymptom)
+class DiagnosisToSymptomAdmin(admin.ModelAdmin):
+    pass
--- symptoms_api/api/models.py
+++ symptoms_api/api/models.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Symptom(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Diagnosis(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class DiagnosisToSymptom(models.Model):
+    diagnosis_id = models.ForeignKey(Diagnosis, on_delete=models.CASCADE)
+    symptom_id = models.ForeignKey(Diagnosis, on_delete=models.CASCADE)
+    times_encountered = models.IntegerField(default=1)
