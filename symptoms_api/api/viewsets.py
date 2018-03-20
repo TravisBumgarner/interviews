af5a68e0416cc1b149f1970faacb5dec332f5e91
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-20 15:11:58 -0400

    Added endpoint for getting diagnoses for symptom

--- symptoms_api/api/viewsets.py
+++ symptoms_api/api/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import SymptomSerializer
+from .serializers import SymptomSerializer, DiagnosisToSymptomSerializer
 from .models import Symptom, Diagnosis, DiagnosisToSymptom
 
 
@@ -9,3 +9,20 @@ class SymptomViewSet(ModelViewSet):
     queryset = Symptom.objects.all()
     serializer_class = SymptomSerializer
     pagination_class = None
+
+    def retrieve(self, request, *args, **kwargs):
+        symptom = Symptom.objects.get(id=kwargs['pk'])
+        diagnoes_to_symptoms = DiagnosisToSymptom.objects.filter(symptom=symptom)
+
+        times_encountered_sum = 0
+        diagnoses_list = {}
+        for d2s in diagnoes_to_symptoms:
+            times_encountered_sum += d2s.times_encountered
+            diagnoses_list[d2s.id] = {
+                'times_encountered': d2s.times_encountered,
+                'name': d2s.diagnosis.name,
+            }
+        return Response({
+            'times_encountered_sum': times_encountered_sum,
+            'diagnoses_list': diagnoses_list,
+        })
