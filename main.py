fafadc8f078fe3fa2ff46c8e842d5dc58dce0900
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-22 13:21:32 -0400

    Added drive_to_idle_point

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ if __name__ == "__main__":
     if max_x <= 0 or max_y <= 0 or type(max_x) != int or type(max_y) != int:
         raise ValueError('Invalid grid_size value')
 
-    p = Passengers(congestion_factor, max_x, max_y)
+    # p = Passengers(congestion_factor, max_x, max_y)
     t = Traxi(max_x, max_y)
+    t.current_location_x = 0
+    t.current_location_y = 0
     while True:
-        new_p = p.generate()
-        if new_p:
-            print(new_p)
-            t.occupants.append(new_p)
-        else:
-            print("nothing new")
-        t.current_status()
-        t.increment_time()
+        t.drive()
+
+    # while True:
+    #     new_p = p.generate()
+    #     if new_p:
+    #         print(new_p)
+    #         t.occupants.append(new_p)
+    #     else:
+    #         print("nothing new")
+    #     t.current_status()
+    #     t.increment_time()
 
 
 
--- traxi.py
+++ traxi.py
@@ -16,17 +16,51 @@ class Traxi:
         self.current_location_x = max_x / 2
         self.current_location_y = max_y / 2
 
-        # Comment: A better way to do this would be to calculate the optimal waiting point based on requests. There
-        # could be a method that every 50 or so rides recalculates the optimal waiting point. For purely random numbers
+        # Comment: A better way to do this would be to calculate the optimal idle point based on requests. There
+        # could be a method that every 50 or so rides recalculates the optimal idle point. For purely random numbers
         # this would be the center of the grid though
-        self.grid_waiting_point_x = max_x / 2
-        self.grid_waiting_point_y = max_y / 2
+        self.grid_idle_point_x = max_x / 2
+        self.grid_idle_point_y = max_y / 2
 
-    def current_status(self):
+        self.status = 'Idling'
+
+    def print_current_status(self):
         print('Time:     {}'.format(self.time))
+        print('Status:   {}'.format(self.status))
         print('Location: ({}, {})'.format(self.current_location_x, self.current_location_y))
         print('Occupants: {}'.format(', '.join([o['name'] for o in self.occupants] if len(self.occupants) else '')))
+        print('\n')
 
     def increment_time(self):
         self.time += 1
         sleep(0.5)
+
+    def drive_to_idle_point(self):
+        distance_away_x = self.grid_idle_point_x - self.current_location_x
+        distance_away_y = self.grid_idle_point_y - self.current_location_y
+
+        should_move = distance_away_x or distance_away_y
+        # Decrease the larger of x or y first
+        should_move_x = abs(distance_away_x) > abs(distance_away_y)
+        if should_move:
+            self.status = 'Driving'
+            # Reduce x or y distance away by +/- 1
+            if should_move_x:
+                self.current_location_x += distance_away_x / abs(distance_away_x)
+            else:
+                self.current_location_y += distance_away_y / abs(distance_away_y)
+        else:
+            self.status = 'Idling'
+
+    def drive_to_dropoff_point(self):
+        pass
+
+    def drive(self):
+        if len(self.occupants):
+            self.drive_to_dropoff_point()
+        else:
+            self.drive_to_idle_point()
+
+        self.print_current_status()
+        self.increment_time()
+
